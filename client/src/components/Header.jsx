4e973976a374c1a14edb35da4077753844a7331c
Gustavo Luis de Vargas <gustavovargas1999@gmail.com>
2023-10-10 21:05:41 -0300

    commit geral

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -66,12 +66,14 @@ function Header() {
         <div className="Header">
             <Navbar />
             <header data-animebefore='top'>
-                <h1 >Do conceito ao design, fazemos seu site ganhar vida, elevando sua presença digital a um novo patamar </h1>
+                <h1>Do conceito ao design, fazemos seu site ganhar vida, elevando sua presença digital a um novo patamar </h1>
                 <p>Nosso foco é criar soluções digitais personalizadas que têm o poder de impulsionar o crescimento exponencial da sua empresa</p>
                 {/*<Link to='/orçamento' className="Button HeaderButton" onClick={scrollTop}>
                     <p>Solicitar um orçamento</p>
                     <img src="./images/flecha.png" alt="" />
     </Link> */}
+
+    
             </header>
         </div>
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from "react"
 
 
-function Navbar () {
+function Navbar() {
     if (window.innerWidth < 768) {
         return (
-        <nav>
-            <div className="HeaderLogo">
-                <img src='./images/HeaderLogo.png' data-animefirst='bottom' alt=''/>
-            </div>
-        </nav>
+            <nav>
+                <div className="HeaderLogo">
+                    <img src='./images/HeaderLogo.png' data-animefirst='bottom' alt='' />
+                </div>
+            </nav>
         )
     } else {
         return (
             <nav>
-            <div className="HeaderLogo">
-                <img src='./images/HeaderLogo.png' data-animefirst='right' alt=''/>
-            </div>
-        </nav>
+                <div className="HeaderLogo">
+                    <img src='./images/HeaderLogo.png' data-animefirst='right' alt='' />
+                </div>
+            </nav>
         )
     }
 }
